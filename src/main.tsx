f6b0aa46bcffd4835972e3bbfe257663ac534cb5
Kumuditha Tharinda Liyanage <140329762+mr-kumuditha@users.noreply.github.com>
2026-09-09 16:19:32 +0530

    feat: bootstrap React application

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@fontsource/ibm-plex-mono/latin-400.css";
+import "./style.css";
+import App from "./App";
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
